b1d53f5009e0e7485635fdf9d21a9d8f44733a3b
Achraf-laff <khemirachraf95@gmail.com>
2025-06-30 12:16:18 +0100

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div style={{ padding: '20px' }}>
       <h1>Welcome to My Portfolio</h1>
       <Image src="/profile.jpg" alt="My Photo" width={200} height={200} />
-      <p>Hi! I'm a web developer passionate about building modern websites.</p>
+      <p>Hi! I am a web developer passionate about building modern websites.</p>
     </div>
   );
 }
